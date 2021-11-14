11eb78d8222c35720fc37b88ce3146cda28bf380
shavak-kansal <shavakkansal@outlook.com>
2021-11-15 04:43:27 +0530

    .

--- Assignment 5/Q1/main.c
+++ Assignment 5/Q1/main.c
@@ -1,5 +1,21 @@
-#include "pthread.h"
-#include ""
+#include <pthread.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct lab {
+    pthread_mutex_t lock;
+    int32_t count;
+    int32_t* ta;
+    int32_t limit;
+};
+
+void course_thread(){
+
+}
+
 int main() {
 
 }
